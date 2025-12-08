9de8b87967c23ff5a893bfc616ea33e61efc63b4
Robert Shoemaker <shorobe@dunwoody.edu>
2025-12-08 15:30:34 -0600

    day 6

---
+++ python code/Day 6.py
@@ -0,0 +1,59 @@
+
+#! Review day
+
+
+# #! 1. Define and calling Python functions
+#* https://docs.python.org/3/library/functions.html 
+
+# #todo print function and len working together
+# print("hello")          #? Prints 'hello'
+# num_char = len("hello") #? Calls on the hello, and has it fall under definition num_char
+# print(num_char)         #? Prints the num_char definition
+
+# #todo How a function works
+# def my_function():      #? Definition name 
+#     print("hello")      #? Do this
+#     print("bye")        #? Then this
+# my_function()           #? Finally do this
+
+# todo Calling functions, you can you use the next provided url for a site to practice.
+# todo https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+# todo https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# * variables for use with link
+# ? move()
+# ? turn_left()
+# * make function for turn_right
+# ? def turn_right():
+# ?     turn_left()
+# ?     turn_left()
+# ?     turn_left()
+# * make function for turn_around
+# ? def turn_around():
+# ?     turn_left()
+# ?     turn_left()
+# ?     turn_left()
+# * make a function for jump()
+# ? def jump():
+# ?     move()
+# ?     turn_left()
+# ?     move()
+# ?     turn_right()
+# ?     move()
+# ?     turn_right()
+# ?     move()
+# ?     turn_left()
+
+
+#! 2. Indentation
+
+# #* how indentation is supposed to looks as part of function.
+# def my_function():
+#     print("Hello")
+#     print("World")
+
+# #* how indentation is NOT supposed to look as part of a function.
+# def my_function():
+#     print("Hello")
+# print("World")          #? World is now on its function
+
+
---
+++ python projects/Day 6 project.py
@@ -0,0 +1,6 @@
+
+#! the PRoject for today will be for you to solve the Reeborg's World maze
+#* https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+#! As the position in the maze changes everytime there will not be a solution present
+#? Good Luck!
